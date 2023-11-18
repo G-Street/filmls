bbdf920959e676c8a5ff62eb41d80ab1623b8499
Jake Ireland <jakewilliami@icloud.com>
2023-11-19 10:27:19 +1300

    Implement subtitle format checker for series

--- src/main.rs
+++ src/main.rs
@@ -457,7 +457,41 @@ fn main() {
             // Count series
             if let Some(check_series_subtitles) = cli.series {
                 if check_series_subtitles {
-                    todo!();
+                    let mut series_dir = dirname.clone();
+                    series_dir.push(constants::SERIES_DIR_NAME);
+                    let series: Vec<_> = fs::read_dir(&series_dir)
+                        .expect(format!("Cannot read directory: {:?}", series_dir).as_str())
+                        .map(|e| e.expect("Cannot retreive file information").path())
+                        .collect();
+                    for path in series {
+                        if path.is_dir() {
+                            for p in fs::read_dir(&path)
+                                .expect("Cannot read directory")
+                                .map(|e| e.unwrap())
+                            {
+                                let p = &p.path();
+                                let p = &p.as_path();
+                                let d = &p
+                                    .file_name()
+                                    .expect("Cannot get file name from file")
+                                    .to_str()
+                                    .unwrap();
+                                if constants::SEASON_RE.is_match(d) {
+                                    for sub in list_subtitles(p) {
+                                        // TODO: warn if subtitle base name does not match film
+                                        if !check_subtitle_format(&sub, &constants::SUB_RE) {
+                                            println!(
+                                                "{}{}{}",
+                                                "Subtitle file ".italic(),
+                                                sub.bold(),
+                                                " is incorrectly formatted".italic()
+                                            );
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
