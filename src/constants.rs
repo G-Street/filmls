53bafebe9d374062a1578b950a2b77d3b87b53d3
Jake Ireland <jakewilliami@icloud.com>
2023-11-19 09:47:52 +1300

    Reformat constand extension types

--- src/constants.rs
+++ src/constants.rs
@@ -6,46 +6,12 @@ pub const SERIES_DIR_NAME: &str = "Series";
 
 // Source: https://blog.filestack.com/thoughts-and-knowledge/complete-list-audio-video-file-formats/
 pub const MEDIA_TYPES: [&str; 18] = [
-    "mkv",
-    "avi",
-    "mp4",
-    "webm",
-    "mpg",
-    "mp2",
-    "mpeg",
-    "mpe",
-    "mpv",
-    "ogg",
-    "m4p",
-    "m4v",
-    "wmv",
-    "mov",
-    "qt",
-    "flv",
-    "swf",
-    "avdchd",
+    "mkv", "avi", "mp4", "webm", "mpg", "mp2", "mpeg", "mpe", "mpv", "ogg", "m4p", "m4v", "wmv",
+    "mov", "qt", "flv", "swf", "avdchd",
 ];
 
 // Source: https://github.com/seanap/Plex-Audiobook-Guide/blob/master/Scripts/BookCopy.sh
 const _AUDIOBOOK_TYPES: [&str; 20] = [
-    "m4b",
-    "mp3",
-    "mp4",
-    "m4a",
-    "ogg",
-    "pdf",
-    "epub",
-    "azw",
-    "azw3",
-    "azw4",
-    "doc",
-    "docx",
-    "m4v",
-    "djvu",
-    "opf",
-    "odt",
-    "pdx",
-    "wav",
-    "mobi",
-    "xls"
+    "m4b", "mp3", "mp4", "m4a", "ogg", "pdf", "epub", "azw", "azw3", "azw4", "doc", "docx", "m4v",
+    "djvu", "opf", "odt", "pdx", "wav", "mobi", "xls",
 ];
